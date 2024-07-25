9b0c1e87bc8daaf1e4eb0706e5d8ea181c406373
Dhyey <dhyeykakadiya1015@gmail.com>
2024-07-26 01:38:48 +0530

    added displaying added takes to task list when added form add tasks

--- cs50example/tasks/views.py
+++ cs50example/tasks/views.py
@@ -12,8 +12,12 @@ class NewTaskForm(forms.Form):
 # Create your views here.
 
 def index(request):
+    
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+        
     context = {
-        'tasks':tasks
+        'tasks':request.session["tasks"]
     }
     return render(request, "tasks/index.html",context)
 
