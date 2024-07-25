059315a9cdde57b4c99f280775dfe7ffa1dc90a7
Dhyey <dhyeykakadiya1015@gmail.com>
2024-07-25 23:07:48 +0530

    added function name index

--- cs50example/tasks/views.py
+++ cs50example/tasks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
+tasks = ["foo", "bar", "baz"]
 # Create your views here.
+
+def index(request):
+    context = {
+        'tasks':tasks
+    }
+    return render(request, "tasks/index.html",context)
+    
