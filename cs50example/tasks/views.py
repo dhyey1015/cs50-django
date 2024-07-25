1715d2a2c6cb2fc02a0e2ef59ae0790f3af2b2d6
Dhyey <dhyeykakadiya1015@gmail.com>
2024-07-26 01:23:44 +0530

    added a server-side validation method, added an httpresponceredirect to redirect to the task list page

--- cs50example/tasks/views.py
+++ cs50example/tasks/views.py
@@ -1,11 +1,14 @@
 from django import forms 
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
-tasks = ['foo', 'bar', 'baz']
+tasks = []
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label= "Priority", min_value=1, max_value=10)
 # Create your views here.
 
 def index(request):
@@ -15,6 +18,19 @@ def index(request):
     return render(request, "tasks/index.html",context)
 
 def add(request):
+    
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["tasks"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("index"))
+            
+        else:
+            context= {
+                "form":form
+            }
+            return render(request, "task/add.html",context)
     
     context = {
         "form": NewTaskForm()
