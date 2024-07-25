09bd0d5a9082e1b45385ce6cd7578225ec20b7d2
Dhyey <dhyeykakadiya1015@gmail.com>
2024-07-25 22:58:10 +0530

    added apath for the tasks app urls(corrected the text)

--- cs50example/cs50example/urls.py
+++ cs50example/cs50example/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include("hello.urls")),
     path('isitnewyear', include("isitnewyear.urls")),
-    path('tasks/', include("tasks.url") )
+    path('tasks/', include("tasks.urls") )
 ]
