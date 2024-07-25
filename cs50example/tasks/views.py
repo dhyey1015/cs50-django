8c3d1b8774684d618a59a666a253a1a35d31c538
Dhyey <dhyeykakadiya1015@gmail.com>
2024-07-26 00:48:52 +0530

    added a class NewTaskForm to create forms

--- cs50example/tasks/views.py
+++ cs50example/tasks/views.py
@@ -1,7 +1,11 @@
+from django import forms 
 from django.shortcuts import render
 
 
 tasks = ['foo', 'bar', 'baz']
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
 # Create your views here.
 
 def index(request):
@@ -11,5 +15,9 @@ def index(request):
     return render(request, "tasks/index.html",context)
 
 def add(request):
-    return render(request, "tasks/add.html")
+    
+    context = {
+        "form": NewTaskForm()
+    }
+    return render(request, "tasks/add.html",context)
     
