8b7baf597de263cea00b09676e62e3f149ad5606
Dhyey <dhyeykakadiya1015@gmail.com>
2024-07-25 21:49:38 +0530

    addeda a function index that will take request as na arg and render the context tell is it new year or not

--- cs50example/isitnewyear/views.py
+++ cs50example/isitnewyear/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
+
+def index(request):
+    context = {
+        "newyear": datetime.datetime(2025,1,1) == datetime.datetime.now()
+    }
+    return render(request, "isitnewyear/index.html",context)
+    
