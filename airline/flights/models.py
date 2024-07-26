91015708cbda3198c2ca5e118fc5c1dc77e667ec
Dhyey <dhyeykakadiya1015@gmail.com>
2024-07-26 14:43:08 +0530

    changes the code model's field

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Airport(models.Model):
-    code = models.IntegerField(max_length= 3)
+    code = models.CharField(max_length= 3)
     city = models.CharField(max_length= 64)
     
     def __str__(self):
@@ -10,8 +10,8 @@ class Airport(models.Model):
 
 
 class Flight(models.Model):
-    origin = models.CharField(max_length= 64)
-    destination = models.CharField(max_length=64)
+    origin = models.ForeignKey(Airport, on_delete= models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport,on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
 
     def __str__(self):
