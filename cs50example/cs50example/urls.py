0ed2ba95db87c69bf264b0d3ccd1ac574e935591
Dhyey <dhyeykakadiya1015@gmail.com>
2024-07-27 16:18:52 +0530

    added a path name section goes to singlepage1 urls

--- cs50example/cs50example/urls.py
+++ cs50example/cs50example/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include("hello.urls")),
     path('isitnewyear', include("isitnewyear.urls")),
-    path('tasks/', include("tasks.urls") )
+    path('tasks/', include("tasks.urls")),
+    path('section/', include("singlepage1.urls")),
 ]
