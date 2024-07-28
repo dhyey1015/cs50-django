1ff6bfdc4d6514ccc6500c071806970e6394864b
Dhyey <dhyeykakadiya1015@gmail.com>
2024-07-28 10:31:00 +0530

    user interfacer lecture changers

--- cs50example/hello/urls.py
+++ cs50example/hello/urls.py
@@ -3,8 +3,13 @@ from . import views
 
 urlpatterns = [
     
-    path("",views.index,name ="index"),
+    #path("",views.index,name ="index"),
     path("dhyey", views.dhyey, name="dhyey"),
-    path("<str:name>", views.greet, name = "greet"),
+    #path("<str:name>", views.greet, name = "greet"),
+    path("singlepage", views.singlepage, name= "singlepage"),
+    path("singlepage2", views.singlepage2, name="singlepage2"),
+    path("sections/<int:num>", views.section, name= "sections"),
+    path("section/<int:num1>", views.section, name='section'),
+    path("scroll", views.scroll, name="scroll")
     
 ]
--- cs50example/hello/views.py
+++ cs50example/hello/views.py
@@ -1,18 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 # Create your views here.
 
 def index(request):
     return render(request, "hello/index.html")
 
 def dhyey(request):
-    return HttpResponse("hello! dhyey")
+   return HttpResponse("hello! dhyey")
 
-def greet(request, name):
-    
+def greet(request, name): 
     context = {
         "name": name
     }
     return render(request, "hello/greet.html",context)
+
+#singel-page all infromation
+
+texts = ["page1beuwinjokl;A", "page2BFUNIEWOMKL,Q", "page3eubinjkwclm"]
+
+def singlepage(request):
+    return render(request, "hello/singlepage.html")
+
+def singlepage2(request):
+    return render(request, "hello/singlepage2.html")
+
+def section(request,num):
+    if 1 <= num <= 3:
+        return HttpResponse(texts[num -1])
+    else:
+        raise Http404("No Such Section!!")
+    
+def scroll(request):
+    return render(request,"hello/scroll.html")
+
+    
 
     
